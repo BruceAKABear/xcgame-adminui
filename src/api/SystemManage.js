3c3cca174e2e7f3f5353443389242aea23844cb9
惠倩 <18391732098@163.com>
2020-09-25 17:22:38 +0800

    1

--- src/api/SystemManage.js
+++ src/api/SystemManage.js
@@ -1,6 +1,10 @@
 // 应用api
 import { createAPI } from '@/utils/request'
-// 新增或修改
-export const addOrUpdate = data => createAPI('/prePayResponse/addOrUpdate', 'post', data)
-// 查询
+// 支付新增或修改
+export const addOrUpdatePay = data => createAPI('/prePayResponse/addOrUpdate', 'post', data)
+// 支付查询
 export const getPrePayResponse = data => createAPI('/prePayResponse/getPrePayResponse', 'get', data)
+// 关注新增或修改
+export const addOrUpdateFollow = data => createAPI('/followResponse/addOrUpdate', 'post', data)
+// 关注查询
+export const getFollowResponse = data => createAPI('/followResponse/getFollowResponse', 'get', data)
--- src/views/SystemManage.vue
+++ src/views/SystemManage.vue
@@ -7,7 +7,7 @@
     <!--支付配置-->
     <el-tabs type="border-card" border style="margin-top: 25px; width: 100%;">
       <el-tab-pane label="充值">
-        <el-form label-position="right" label-width="120px" :model="addFromDataPay" ref="addFromDataPayFrom">
+        <el-form label-position="right" label-width="120px" :model="addFromDataPay" ref="addFromDataPayFrom" :rules="rules">
           <el-form-item label="标题" prop="title">
             <el-input autocomplete="off" v-model="addFromDataPay.title"></el-input>
           </el-form-item>
@@ -32,7 +32,7 @@
             </el-upload>
           </el-form-item>
           <el-form-item>
-            <el-button type="primary" @click="submitForm('addFromDataPayFrom')">提 交</el-button>
+            <el-button type="primary" @click="submitFormPay('addFromDataPayFrom')">提 交</el-button>
           </el-form-item>
           <el-form-item>
             <el-card class="box-card" body-style="padding:5px" style="width:230px;height: 105px;">
@@ -54,22 +54,48 @@
       </el-tab-pane>
       <!--关注配置-->
       <el-tab-pane label="关注">
-        <el-form label-position="right" label-width="120px" ref="addForm">
+        <el-form label-position="right" label-width="120px" ref="addFromDataFollow"  :model="addFromDataFollow" :rules="rules">
           <el-form-item label="标题" prop="title">
-            <el-input autocomplete="off"></el-input>
+            <el-input autocomplete="off" v-model="addFromDataFollow.title"></el-input>
           </el-form-item>
-          <el-form-item label="描述" prop="type">
-            <el-input autocomplete="off"></el-input>
+          <el-form-item label="描述" prop="title">
+            <el-input autocomplete="off" v-model="addFromDataFollow.descrip"></el-input>
           </el-form-item>
           <el-form-item label="图片">
-            <el-upload class="upload-demo" name="file" :action="imageUploadUrl" :limit="1" :headers="uploadHeader"
-                       :on-remove="handleRemovePic" :file-list="picList" list-type="picture">
+            <el-upload
+              class="upload-demo"
+              :action="imageUploadUrl"
+              name="file"
+              :limit="1"
+              :headers="uploadHeader"
+              :on-remove="handleRemovePic"
+              :file-list="picList"
+              :on-success="successUpload"
+              list-type="picture"
+              :before-upload="beforeUpload"
+            >
               <el-button size="small" type="primary">点击上传</el-button>
-              <div slot="tip" class="el-upload__tip">只能上传一个图片，同时只能上传jpg/png文件，且不超过10MB</div>
+              <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过10MB</div>
             </el-upload>
           </el-form-item>
           <el-form-item>
-            <el-button type="primary">提 交</el-button>
+            <el-button type="primary" @click="submitFormFollow('addFromDataFollow')">提 交</el-button>
+          </el-form-item>
+           <el-form-item>
+            <el-card class="box-card" body-style="padding:5px" style="width:230px;height: 105px;">
+              <div class="count-item">
+                <div class="count-item-content">
+                  <span style="height: 25px;line-height: 20px;">{{addFromDataFollow.title}}</span>
+                  <span style="height: 70px;font-size: 12px;color: gray;line-height: 25px;">{{addFromDataFollow.descrip}}</span>
+                </div>
+                <div class="img-box">
+                  <el-image
+                    src="https://img11.360buyimg.com/n7/jfs/t1/94448/29/2734/524808/5dd4cc16E990dfb6b/59c256f85a8c3757.jpg"
+                    fit="cover"
+                    style="width: 80px; height: 80px"></el-image>
+                </div>
+              </div>
+            </el-card>
           </el-form-item>
         </el-form>
       </el-tab-pane>
@@ -78,7 +104,7 @@
 </template>
 
 <script>
-import { addOrUpdate, getPrePayResponse } from '@/api/SystemManage'
+import { addOrUpdatePay, getPrePayResponse, addOrUpdateFollow } from '@/api/SystemManage'
 import { getToken } from '@/utils/TokenUtil'
 
 export default {
@@ -86,7 +112,32 @@ export default {
   data () {
     return {
       addFromDataPay: {},
+      addFromDataFollow: {},
       picList: [],
+      payList: {},
+      rules: {
+        title: [
+          {
+            required: true,
+            message: '请输入内容标题',
+            trigger: 'blur'
+          }
+        ],
+        descrip: [
+          {
+            required: true,
+            message: '请输入描述内容',
+            trigger: 'blur'
+          }
+        ],
+        picUrl: [
+          {
+            required: true,
+            message: '请上传图片',
+            trigger: 'change'
+          }
+        ]
+      },
       uploadHeader: {
         token: '321'
       },
@@ -94,17 +145,14 @@ export default {
     }
   },
   methods: {
-    handleEdit (row) {
-      this.addFromDataPay = row
-      this.picList.push({ url: row.picUrl })
-    },
-    submitForm (formName) {
+    // 支付新增
+    submitFormPay (formName) {
       console.log('-------')
       this.$refs[formName].validate((valid) => {
         if (valid) {
           // 新增请求 [0].url
           this.addFromDataPay.picUrl = this.picList[0].url
-          addOrUpdate(this.addFromDataPay).then(res => {
+          addOrUpdatePay(this.addFromDataPay).then(res => {
             this.$message.success('新增成功')
             // 清空
             this.$refs[formName].resetFields()
@@ -117,6 +165,34 @@ export default {
         }
       })
     },
+    // 关注新增
+    submitFormFollow (formName) {
+      console.log('-------')
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          // 新增请求 [0].url
+          this.addFromDataFollow.picUrl = this.picList[0].url
+          addOrUpdateFollow(this.addFromDataFollow).then(res => {
+            this.$message.success('新增成功')
+            // 清空
+            this.$refs[formName].resetFields()
+            this.picList = []
+            // this.doQueryItemList()
+          })
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    },
+    // 支付查询
+    doQueryPay () {
+      getPrePayResponse({}).then(res => {
+        console.log('查询支付：', res.data)
+        this.payList = res.data
+      })
+    },
+    // 关注查询
     handlePreview (file) {
       console.log(file)
     },
@@ -141,15 +217,10 @@ export default {
     },
     setToken () {
       this.uploadHeader.token = getToken()
-    },
-    doQueryItemList () {
-      getPrePayResponse().then(res => {
-        this.itemList = res.data
-      })
     }
   },
   created () {
-    this.doQueryItemList()
+    this.doQueryPay()
     this.setToken()
   }
 }
